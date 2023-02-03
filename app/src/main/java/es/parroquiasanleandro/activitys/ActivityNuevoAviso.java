9a2c2495eff812e3d3e6214d37b859e26dd67595
renedogj <renedogj@gmail.com>
2023-02-03 16:09:33 +0100

    add comentado subirArchivos, add ProgressDialog

--- app/src/main/java/es/parroquiasanleandro/activitys/ActivityNuevoAviso.java
+++ app/src/main/java/es/parroquiasanleandro/activitys/ActivityNuevoAviso.java
@@ -2,11 +2,13 @@ package es.parroquiasanleandro.activitys;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -27,6 +29,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
@@ -38,8 +41,10 @@ import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import es.parroquiasanleandro.Aviso;
@@ -53,13 +58,16 @@ import es.renedogj.fecha.Mes;
 public class ActivityNuevoAviso extends AppCompatActivity {
     Context context = ActivityNuevoAviso.this;
 
+    private static final int SELECT_FILE_REQUEST_CODE = 100;
+
     private EditText etTitulo;
     private ImageView ivImagenAviso;
     private ConstraintLayout cntrtlytAñadirImagen;
     private ImageButton btnEliminarImagen;
     private TextView tvAñadirImagen;
     private EditText etDescripcion;
-    private TextView etUrl;
+    private EditText etUrl;
+    //private TextView tvArchivo;
     private TextView tvFechaInicio;
     private TextView tvHoraInicio;
     private LinearLayout lnlytFechaFinal;
@@ -76,6 +84,9 @@ public class ActivityNuevoAviso extends AppCompatActivity {
 
     private String imagenStringByte = null;
     private String nombreImagen = null;
+
+    //private String filePath = null;
+    //List<Bitmap> bitmaps = null;
 
     private Fecha fechaInicio;
     private Fecha fechaFin;
@@ -96,6 +107,7 @@ public class ActivityNuevoAviso extends AppCompatActivity {
         tvAñadirImagen = findViewById(R.id.tvAñadirImagen);
         etDescripcion = findViewById(R.id.etDescripcion);
         etUrl = findViewById(R.id.etUrl);
+        //tvArchivo = findViewById(R.id.tvArchivo);
         tvFechaInicio = findViewById(R.id.tvFechaInicio);
         tvHoraInicio = findViewById(R.id.tvHoraInicio);
         lnlytFechaFinal = findViewById(R.id.lnlytFechaFinal);
@@ -202,8 +214,9 @@ public class ActivityNuevoAviso extends AppCompatActivity {
         cntrtlytAñadirImagen.setOnClickListener(v -> {
             Intent intent = new Intent(context, ActivitySeleccionarImagen.class);
             intent.putExtra("idGrupo", usuario.getGruposAdministrados()[spinnerGrupo.getSelectedItemPosition()].id);
-            startActivityForResult(intent, 1);
+            startActivityForResult(intent, 0);
         });
+
 
         btnEliminarImagen.setOnClickListener(v -> {
             imagenStringByte = null;
@@ -211,6 +224,13 @@ public class ActivityNuevoAviso extends AppCompatActivity {
             Glide.with(context).clear(ivImagenAviso);
             btnEliminarImagen.setVisibility(View.GONE);
         });
+
+        /*tvArchivo.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/pdf");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(Intent.createChooser(intent, "Select a PDF"), SELECT_FILE_REQUEST_CODE);
+        });*/
 
         if (idAviso != null) {
             Volley.newRequestQueue(context).add(new StringRequest(Request.Method.POST, Url.obtenerAviso, result -> {
@@ -300,28 +320,34 @@ public class ActivityNuevoAviso extends AppCompatActivity {
         }
 
         if (aviso.titulo.length() > 0) {
-            //if (aviso.descripcion.length() > 0) {
-                if (aviso.getFechaInicio().esIgualA(aviso.getFechaFin()) || Fecha.isFecha1MayorQueFecha2(aviso.getFechaFin(), aviso.getFechaInicio())) {
-                    if (idAviso == null) {
-                        guardarNuevoAviso(aviso);
-                    } else {
-                        editarAviso(aviso);
-                    }
-                    finish();
+            if (aviso.getFechaInicio().esIgualA(aviso.getFechaFin()) || Fecha.isFecha1MayorQueFecha2(aviso.getFechaFin(), aviso.getFechaInicio())) {
+                if (idAviso == null) {
+                    guardarNuevoAviso(aviso);
                 } else {
-                    Toast.makeText(context, "La fecha final no puede ser anterior a la fecha inicial", Toast.LENGTH_SHORT).show();
+                    editarAviso(aviso);
                 }
-            //} else {
-            //    Toast.makeText(context, "El campo de descripción no puede estar vacio", Toast.LENGTH_SHORT).show();
-            //}
+            } else {
+                Toast.makeText(context, "La fecha final no puede ser anterior a la fecha inicial", Toast.LENGTH_SHORT).show();
+            }
         } else {
             Toast.makeText(context, "El campo de titulo no puede estar vacio", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void guardarNuevoAviso(Aviso aviso) {
+        /*if(filePath != null){
+            // Use the file path to start the upload task
+            UploadFileTask uploadFileTask = new UploadFileTask();
+            uploadFileTask.setIdGrupo(aviso.idGrupo);
+            uploadFileTask.execute(filePath);
+        }*/
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Guardando el aviso, por favor espera...");
+        progressDialog.show();
+
         RequestQueue requestQueue = Volley.newRequestQueue(context);
         requestQueue.add(new StringRequest(Request.Method.POST, Url.crearNuevoAviso, result -> {
+            progressDialog.dismiss();
             Log.e("RESULT", result);
             try {
                 JSONObject jsonResult = new JSONObject(result);
@@ -330,10 +356,12 @@ public class ActivityNuevoAviso extends AppCompatActivity {
                 } else {
                     Toast.makeText(context, "Se ha producido un error al crear el aviso, por favor intentalo más tarde", Toast.LENGTH_SHORT).show();
                 }
+                finish();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }, error -> {
+            progressDialog.dismiss();
             Toast.makeText(context, "Se ha producido un error al conectar con el servidor", Toast.LENGTH_SHORT).show();
             error.printStackTrace();
         }) {
@@ -342,14 +370,30 @@ public class ActivityNuevoAviso extends AppCompatActivity {
                 if (imagenStringByte != null) {
                     parametros.put(Aviso.IMAGEN, imagenStringByte);
                 }
+                /*if (bitmaps != null){
+                    for (int i = 0; i < bitmaps.size(); i++) {
+                        Bitmap bitmap = bitmaps.get(i);
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+                        String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+                        parametros.put("image_" + i, imageString);
+                    }
+                    parametros.put("file_name",System.currentTimeMillis() +".pdf");
+                }*/
                 return parametros;
             }
-        });
+        }.setRetryPolicy(new DefaultRetryPolicy(20 * 1000 * 10, 1, 1.0f)));
     }
 
     public void editarAviso(Aviso aviso) {
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Guardando el aviso, por favor espera...");
+        progressDialog.show();
+
         RequestQueue requestQueue = Volley.newRequestQueue(context);
         requestQueue.add(new StringRequest(Request.Method.POST, Url.editarAviso, result -> {
+            progressDialog.dismiss();
             Log.e("RESULT", result);
             try {
                 JSONObject jsonResult = new JSONObject(result);
@@ -358,10 +402,12 @@ public class ActivityNuevoAviso extends AppCompatActivity {
                 } else {
                     Toast.makeText(context, "Se ha producido un error al editar el aviso, por favor intentalo más tarde", Toast.LENGTH_SHORT).show();
                 }
+                finish();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }, error -> {
+            progressDialog.dismiss();
             Toast.makeText(context, "Se ha producido un error al conectar con el servidor", Toast.LENGTH_SHORT).show();
             error.printStackTrace();
         }) {
@@ -377,7 +423,12 @@ public class ActivityNuevoAviso extends AppCompatActivity {
     }
 
     public void eliminarAviso() {
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Eliminando el aviso, por favor espera...");
+        progressDialog.show();
+
         Volley.newRequestQueue(context).add(new StringRequest(Request.Method.POST, Url.eliminarAviso, result -> {
+            progressDialog.dismiss();
             try {
                 JSONObject jsonResult = new JSONObject(result);
                 if (!jsonResult.getBoolean("error")) {
@@ -385,12 +436,14 @@ public class ActivityNuevoAviso extends AppCompatActivity {
                 } else {
                     Toast.makeText(context, "Se ha producido un error al eliminar el aviso", Toast.LENGTH_SHORT).show();
                 }
+                finish();
             } catch (JSONException e) {
                 Toast.makeText(context, "Se ha producido un error en el servidor al eliminar el aviso", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
                 onBackPressed();
             }
         }, error -> {
+            progressDialog.dismiss();
             Toast.makeText(context, "Se ha producido un error al conectar con el servidor", Toast.LENGTH_SHORT).show();
         }) {
             @Override
@@ -406,29 +459,70 @@ public class ActivityNuevoAviso extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == ActivitySeleccionarImagen.SELECION_IMAGEN_GALERIA) {
-            if (requestCode == 1 && data != null && data.getData() != null) {
-                Uri uriImagen = data.getData();
-                try {
-                    Bitmap bitmapImagen = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uriImagen);
-                    imagenStringByte = bitmapToString(bitmapImagen);
-                    Glide.with(context).load(uriImagen.toString()).into(ivImagenAviso);
-                    btnEliminarImagen.setVisibility(View.VISIBLE);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                nombreImagen = generarNombreImagen();// + "." + getFileExtension(uriImagen);
-            }
-        }
-        if (resultCode == ActivitySeleccionarImagen.SELECION_IMAGEN_SERVIDOR) {
-            nombreImagen = data.getStringExtra("nombreImagen");
-            String idGrupo = usuario.getGruposAdministrados()[spinnerGrupo.getSelectedItemPosition()].id;
-            Glide.with(context).load(Url.obtenerImagenAviso + idGrupo + "/img/" + nombreImagen).into(ivImagenAviso);
-            btnEliminarImagen.setVisibility(View.VISIBLE);
-        }
+        if (requestCode == SELECT_FILE_REQUEST_CODE) {
+            /*Uri uriFile = data.getData();
+            // Get the file path from the URI
+            //filePath = getFilePathFromUri(uriFile);
+            //tvArchivo.setText(filePath);
 
-        tvAñadirImagen.setText("Cambiar imagen");
-        ivImagenAviso.setPaddingRelative(25, 5, 25, 5);
+            bitmaps = getBitmapsFromPdf(uriFile);
+            tvArchivo.setText("Archivo selecionado con exito");
+            Log.e("BIT MAPS",bitmaps.toString());
+            Log.e("BIT MAPS",bitmaps.size() + "");*/
+        } else {
+            if (resultCode == ActivitySeleccionarImagen.SELECION_IMAGEN_GALERIA) {
+                if (requestCode == 1 && data != null && data.getData() != null) {
+                    Uri uriImagen = data.getData();
+                    try {
+                        Bitmap bitmapImagen = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uriImagen);
+                        imagenStringByte = bitmapToString(bitmapImagen);
+                        Glide.with(context).load(uriImagen.toString()).into(ivImagenAviso);
+                        btnEliminarImagen.setVisibility(View.VISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    nombreImagen = generarNombreImagen();// + "." + getFileExtension(uriImagen);
+                }
+            }
+            if (resultCode == ActivitySeleccionarImagen.SELECION_IMAGEN_SERVIDOR) {
+                nombreImagen = data.getStringExtra("nombreImagen");
+                String idGrupo = usuario.getGruposAdministrados()[spinnerGrupo.getSelectedItemPosition()].id;
+                Glide.with(context).load(Url.obtenerImagenAviso + idGrupo + "/img/" + nombreImagen).into(ivImagenAviso);
+                btnEliminarImagen.setVisibility(View.VISIBLE);
+            }
+
+            tvAñadirImagen.setText("Cambiar imagen");
+            ivImagenAviso.setPaddingRelative(25, 5, 25, 5);
+        }
+    }
+
+    public List<Bitmap> getBitmapsFromPdf(Uri pdfUri) {
+        List<Bitmap> bitmaps = new ArrayList<>();
+        PdfRenderer pdfRenderer = null;
+        try {
+            pdfRenderer = new PdfRenderer(getContentResolver().openFileDescriptor(pdfUri, "r"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        try {
+            //ParcelFileDescriptor fileDescriptor = ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY);
+            //PdfRenderer pdfRenderer = new PdfRenderer(fileDescriptor);
+            for (int i = 0; i < pdfRenderer.getPageCount(); i++) {
+                PdfRenderer.Page page = pdfRenderer.openPage(i);
+                int width = context.getResources().getDisplayMetrics().densityDpi / 72 * page.getWidth();
+                int height = context.getResources().getDisplayMetrics().densityDpi / 72 * page.getHeight();
+                Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+                page.close();
+                bitmaps.add(bitmap);
+            }
+            pdfRenderer.close();
+            //fileDescriptor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  bitmaps;
     }
 
     private String bitmapToString(Bitmap bitmap) {
@@ -447,4 +541,26 @@ public class ActivityNuevoAviso extends AppCompatActivity {
     private String generarNombreImagen() {
         return System.currentTimeMillis() + "_" + usuario.getGruposAdministrados()[spinnerGrupo.getSelectedItemPosition()].id + "_" + usuario.getId() + ".jpeg";
     }
+
+    /*private String getFilePathFromUri(Uri uri) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return getFilePathFromUriKitKat(uri);
+        } else {
+            return uri.getPath();
+        }
+    }*/
+
+    /*@TargetApi(Build.VERSION_CODES.KITKAT)
+    private String getFilePathFromUriKitKat(Uri uri) {
+        String filePath = null;
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            String documentId = DocumentsContract.getDocumentId(uri);
+            String[] split = documentId.split(":");
+            String type = split[0];
+            if ("primary".equalsIgnoreCase(type)) {
+                filePath = Environment.getExternalStorageDirectory() + "/" + split[1];
+            }
+        }
+        return filePath;
+    }*/
 }
