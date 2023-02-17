a00be66c50b271b2a8ba19cdc36fa90f4aedca7d
renedogj <renedogj@gmail.com>
2023-02-17 18:12:04 +0100

    add activity configuracion

---
+++ app/src/main/java/es/parroquiasanleandro/activitys/ActivityConfiguracion.java
@@ -0,0 +1,16 @@
+package es.parroquiasanleandro.activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import es.parroquiasanleandro.R;
+
+public class ActivityConfiguracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+    }
+}
